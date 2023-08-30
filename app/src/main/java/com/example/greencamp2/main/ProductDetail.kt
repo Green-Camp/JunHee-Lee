a23576057ee258b477ce43cc293c68dd83e3df71
l2zh <dlwnsgml0109@naver.com>
2023-08-30 22:02:12 +0900

    스터디 2주차

---
+++ app/src/main/java/com/example/greencamp2/main/ProductDetail.kt
@@ -0,0 +1,12 @@
+package com.example.greencamp2.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.greencamp2.R
+
+class ProductDetail : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_detail)
+    }
+}
